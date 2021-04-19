23edd620b5b873922b4a170585e20c1f0abb916a
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-19 15:09:28 +0800

    modify

--- 上課內容/web2/main.py
+++ 上課內容/web2/main.py
@@ -1,8 +1,13 @@
 from flask import Flask,render_template
 app = Flask(__name__)
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return  render_template("login.html")
 
 if __name__ == "__main__":
     app.debug = True
