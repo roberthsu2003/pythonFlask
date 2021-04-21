5b7373845b5dbd5c1d43613013142f80949991f1
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-21 14:08:08 +0800

    modify

--- 上課內容/web3/app/__init__.py
+++ 上課內容/web3/app/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask,render_template
 
 def createApp():
     app = Flask(__name__)
     @app.route("/")
     def index():
-        return "Hello App"
+        return render_template("index.html")
     return app
