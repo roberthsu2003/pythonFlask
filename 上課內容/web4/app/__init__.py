d228ac9ed865b83abf928d483f1d2862036031fa
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-22 14:00:44 +0800

    modify

--- 上課內容/web4/app/__init__.py
+++ 上課內容/web4/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template
 
 def creatApp():
     app = Flask(__name__)
-    @app.route("/")
+    @app.route("/",methods=["GET","POST"])
     def index():
         return render_template("index.html"),200
     return app
--- 上課內容/web4/main.py
+++ 上課內容/web4/main.py
@@ -3,4 +3,5 @@ from app import creatApp
 if __name__ == "__main__":
     app = creatApp()
     app.debug = True
+    #print(app.url_map)
     app.run()
