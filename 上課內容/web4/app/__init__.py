115dace2b1805a30d48a7032fbbb277e5181b300
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-23 09:28:02 +0800

    modify

--- 上課內容/web4/app/__init__.py
+++ 上課內容/web4/app/__init__.py
@@ -6,6 +6,10 @@ def creatApp():
     def index():
         return render_template("index.html"),200
 
+    @app.route("/username", methods=["GET", "POST"])
+    def name():
+        return render_template("username.html",), 200
+
     @app.route("/username/<name>",methods=["GET","POST"])
     def username(name):
         print(f"Hello!{name}")
