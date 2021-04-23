29a558a2e9a86a53afe560409c93c1e44c2a9514
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-23 15:01:30 +0800

    modify

--- 上課內容/web4/app/__init__.py
+++ 上課內容/web4/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+import random
 
 def creatApp():
     app = Flask(__name__)
@@ -16,5 +17,6 @@ def creatApp():
 
     @app.route('/table')
     def table():
-        return render_template("table.html")
+        students = [[random.randint(50,100) for _ in range(5)] for _ in range(50)]
+        return render_template("table.html",students = students),200
     return app
