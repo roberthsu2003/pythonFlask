fbaf19ebe5c1fd5a1200a7976326fcc95faeda4d
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-26 16:15:47 +0800

    modify

--- 上課內容/web5/app/main/views.py
+++ 上課內容/web5/app/main/views.py
@@ -1,10 +1,16 @@
 from . import main
 from flask import render_template
+from random import randint
 
 @main.route("/")
 def index():
     return render_template("main/index.html"),200
 
 @main.route("/lot/<num>")
-def lot(num):
-    return render_template("main/lot.html",num=num),200
+def lotWebPage(num):
+    lot = set()
+    while(len(lot)<=7):
+        lot.add(randint(1, 49))
+    lotlist = list(lot)
+    specialNum = lotlist.pop()
+    return render_template("main/lot.html",num=num,lot=lotlist,specialNum=specialNum),200
