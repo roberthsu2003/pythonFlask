abe1abd8680fbdc66dc1a02389e85090d77ceecf
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-26 15:49:17 +0800

    modify

--- 上課內容/web5/app/main/views.py
+++ 上課內容/web5/app/main/views.py
@@ -4,3 +4,7 @@ from flask import render_template
 @main.route("/")
 def index():
     return render_template("main/index.html"),200
+
+@main.route("/lot/<num>")
+def lot(num):
+    return render_template("main/lot.html",num=num),200
