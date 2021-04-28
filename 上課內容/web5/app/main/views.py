12701d7e200478ac9712632a4b74b31921cdf3ef
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-28 15:30:02 +0800

    modify

--- 上課內容/web5/app/main/views.py
+++ 上課內容/web5/app/main/views.py
@@ -1,17 +1,19 @@
 from . import main
 from .tools import getLot
-
 from flask import render_template
+from flask import request
 
 @main.route("/")
 def index():
     return render_template("main/index.html"),200
 
-@main.route("/lot",methods=["POST", "GET"])
-def lot():
-    return "Hello! World"
 
-@main.route("/lot/<int:num>")
-def lotWebPage(num):
+@main.route("/lot",methods=["POST","GET"])
+def lotWebPage():
+    try:
+        num = int(request.values['count'])
+    except:
+        num = 12
+
     lots = [getLot() for _ in range(num)]
     return render_template("main/lot.html",num=num,lots=lots),200
