df2f652cb7168e0619d7edaf4a2de5d9c5f1b3ec
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-29 17:35:13 +0800

    modify

--- 上課內容/web5/app/form/views.py
+++ 上課內容/web5/app/form/views.py
@@ -1,6 +1,12 @@
 from . import form
-from flask import render_template
+from flask import render_template,request
 
 @form.route("/")
 def form():
+    email = request.args.get('email')
+    desc = request.args.get('desc')
+
+    if email and desc:
+        print(email)
+        print(desc)
     return render_template("/form/index.html")
