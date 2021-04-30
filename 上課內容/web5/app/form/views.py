7df83960a0a02b5758d3e9980ed1ddefb0ecbb16
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-30 13:46:37 +0800

    modify

--- 上課內容/web5/app/form/views.py
+++ 上課內容/web5/app/form/views.py
@@ -5,8 +5,16 @@ from flask import render_template,request
 def form():
     email = request.args.get('email')
     desc = request.args.get('desc')
+    overnight = request.args.get('overnight')
 
-    if email and desc:
-        print(email)
-        print(desc)
-    return render_template("/form/index.html")
+    class Person():
+        def __init__(self,email=None,desc=None,overnight=None):
+            self.email = email
+            self.desc = desc
+            self.overnight = overnight
+
+    p1=Person(email=email, desc=desc, overnight=overnight)
+
+
+
+    return render_template("/form/index.html",person = p1), 200
