5534cc4f93440a826edae3499fe520168f71e6ce
roberthsu2003 <roberthsu2003@gmail.com>
2021-04-30 14:15:47 +0800

    modify

--- 上課內容/web5/app/form/views.py
+++ 上課內容/web5/app/form/views.py
@@ -1,20 +1,20 @@
 from . import form
 from flask import render_template,request
 
-@form.route("/")
+@form.route("/",methods=["GET","POST"])
 def form():
-    email = request.args.get('email')
-    desc = request.args.get('desc')
-    overnight = request.args.get('overnight')
+        class Person():
+            def __init__(self, email=None, desc=None, overnight=None):
+                self.email = email
+                self.desc = desc
+                self.overnight = overnight
 
-    class Person():
-        def __init__(self,email=None,desc=None,overnight=None):
-            self.email = email
-            self.desc = desc
-            self.overnight = overnight
+        if request.method == "POST":
+            email = request.values['email']
+            desc = request.values['desc']
+            overnight = request.values['overnight']
 
-    p1=Person(email=email, desc=desc, overnight=overnight)
-
-
-
-    return render_template("/form/index.html",person = p1), 200
+            p1=Person(email=email, desc=desc, overnight=overnight)
+            return render_template("/form/index.html",person=p1), 200
+        else:
+            return render_template("/form/index.html"), 200
