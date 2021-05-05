f0bc6b86b99c82789d9902af1c439576c0e02738
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-05 17:04:27 +0800

    modify

--- 上課內容/web6/app/model.py
+++ 上課內容/web6/app/model.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from . import db
 
 class City(db.Model):
@@ -21,6 +22,12 @@ def createDB():
     sqlitePath = os.path.join(basedir,'cities.sqlite')
     if not os.path.exists(sqlitePath):
         print("下載資料")
+        response = requests.get('https://flask-robert.herokuapp.com/city')
+        allData=response.json()
+        allCity = allData['allCity']
+        for city in allCity:
+            print(city)
+            print("==========")
         #db.create_all()
     else:
         print('已經建立')
