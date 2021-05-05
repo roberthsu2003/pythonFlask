e8227ff89724c7775bd27a2d9c05c95b48031291
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-05 15:22:12 +0800

    modify

--- 上課內容/web6/app/__init__.py
+++ 上課內容/web6/app/__init__.py
@@ -1,7 +1,17 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def creatApp():
     app = Flask(__name__)
+    #註冊Blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    #初始化flask_sqlalchemy
+    basedir = os.path.abspath(os.path.dirname(__name__))
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'cities.sqlite')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
     return app
--- 上課內容/web6/app/model.py
+++ 上課內容/web6/app/model.py
@@ -1,1 +1,15 @@
-from flask_sqlalchemy import SQLAlchemy
+from . import db
+
+class City(db.Model):
+    __tablename__ = "city"
+    id = db.Column(db.Integer, primary_key=True)
+    cityName = db.Column(db.String(15))
+    continent = db.Column(db.String(15))
+    country = db.Column(db.String(15))
+    image = db.Column(db.String)
+    lat = db.Column(db.Float)
+    lon = db.Column(db.Float)
+    url = db.Column(db.String)
+
+    def __repr__(self):
+        return f"<City cityName={self.cityName}>"
