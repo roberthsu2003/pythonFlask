4c26464b842dad4c015916081c9a06a5f3e0771c
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-05 13:20:30 +0800

    modify

---
+++ 上課內容/web6/app/__init__.py
@@ -0,0 +1,5 @@
+from flask import Flask
+
+def creatApp():
+    app = Flask(__name__)
+    return app
---
+++ 上課內容/web6/root.py
@@ -0,0 +1,6 @@
+from app import creatApp
+
+app = creatApp()
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
