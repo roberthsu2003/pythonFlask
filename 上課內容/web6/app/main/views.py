865ddfef3ea9962f362ee7f9219846dc49097466
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-06 17:44:05 +0800

    modify

--- 上課內容/web6/app/main/views.py
+++ 上課內容/web6/app/main/views.py
@@ -1,5 +1,5 @@
 from . import main
-from flask import render_template
+from flask import render_template,redirect
 from ..model import db,createDB,City
 
 
@@ -7,21 +7,7 @@ from ..model import db,createDB,City
 @main.route("/")
 def index():
     createDB()
-    cityPageQuery = City.query.paginate(per_page=9, page=1)
-    page = cityPageQuery.page
-    pages = cityPageQuery.pages
-    has_next = cityPageQuery.has_next
-    next_num = None
-    if has_next:
-        next_num = cityPageQuery.next_num
-
-    has_prev = cityPageQuery.has_prev
-    prev_num = None
-    if has_prev:
-        prev_num = cityPageQuery.prev_num
-
-    return render_template('index.html', citys=cityPageQuery.items, has_next=has_next, has_prev=has_prev,
-                           next_num=next_num, prev_num=prev_num, page=page, pages=pages), 200
+    return redirect('/1')
 
 
 @main.route("/<int:pageNum>")
