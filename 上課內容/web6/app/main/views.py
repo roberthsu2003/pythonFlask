ee1ef0350cecb97395c5c79c6e7251d125b13452
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-06 16:18:40 +0800

    modify

--- 上課內容/web6/app/main/views.py
+++ 上課內容/web6/app/main/views.py
@@ -10,6 +10,18 @@ def index():
     return render_template('index.html',citys=citys),200
 
 
-@main.route("/second")
-def second():
-    return render_template('second.html'),200
+@main.route("/<int:pageNum>")
+def page(pageNum):
+    cityPageQuery = City.query.paginate(per_page=9,page=pageNum)
+    print(cityPageQuery.items)
+    has_next = cityPageQuery.has_next
+    next_num = None
+    if has_next:
+        next_num = cityPageQuery.next_num
+
+    has_prev = cityPageQuery.has_prev
+    prev_num = None
+    if has_prev:
+        prev_num = cityPageQuery.prev_num
+
+    return render_template('index.html',citys=cityPageQuery.items,has_next=has_next,has_prev=has_prev,next_num = next_num, prev_num=prev_num),200
