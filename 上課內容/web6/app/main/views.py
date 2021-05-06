bbeee66ad0582c3064297953ecf786ea53be34e5
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-06 17:36:52 +0800

    modify

--- 上課內容/web6/app/main/views.py
+++ 上課內容/web6/app/main/views.py
@@ -3,17 +3,13 @@ from flask import render_template
 from ..model import db,createDB,City
 
 
+
 @main.route("/")
 def index():
     createDB()
-    citys = City.query.limit(9).all()
-    return render_template('index.html',citys=citys),200
-
-
-@main.route("/<int:pageNum>")
-def page(pageNum):
-    cityPageQuery = City.query.paginate(per_page=9,page=pageNum)
-    print(cityPageQuery.items)
+    cityPageQuery = City.query.paginate(per_page=9, page=1)
+    page = cityPageQuery.page
+    pages = cityPageQuery.pages
     has_next = cityPageQuery.has_next
     next_num = None
     if has_next:
@@ -24,4 +20,26 @@ def page(pageNum):
     if has_prev:
         prev_num = cityPageQuery.prev_num
 
-    return render_template('index.html',citys=cityPageQuery.items,has_next=has_next,has_prev=has_prev,next_num = next_num, prev_num=prev_num),200
+    return render_template('index.html', citys=cityPageQuery.items, has_next=has_next, has_prev=has_prev,
+                           next_num=next_num, prev_num=prev_num, page=page, pages=pages), 200
+
+
+@main.route("/<int:pageNum>")
+def page(pageNum):
+
+    cityPageQuery = City.query.paginate(per_page=9,page=pageNum)
+
+
+    page = cityPageQuery.page
+    pages = cityPageQuery.pages
+    has_next = cityPageQuery.has_next
+    next_num = None
+    if has_next:
+        next_num = cityPageQuery.next_num
+
+    has_prev = cityPageQuery.has_prev
+    prev_num = None
+    if has_prev:
+        prev_num = cityPageQuery.prev_num
+
+    return render_template('index.html',citys=cityPageQuery.items,has_next=has_next,has_prev=has_prev,next_num = next_num, prev_num=prev_num,page=page,pages=pages),200
