c06f4c3349abcf073f83ae050ca60a928e776638
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-10 14:56:35 +0800

    modify

--- 上課內容/web6/app/__init__.py
+++ 上課內容/web6/app/__init__.py
@@ -9,6 +9,9 @@ def creatApp():
     #註冊Blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/auth')
     #初始化flask_sqlalchemy
     basedir = os.path.abspath(os.path.dirname(__file__))
 
