b1545828e52e5b713042424610934f5396aa766a
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-10 17:14:50 +0800

    modify

--- 上課內容/web6/app/auth/views.py
+++ 上課內容/web6/app/auth/views.py
@@ -2,7 +2,8 @@ from . import auth
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField,SubmitField,PasswordField
 from wtforms.validators import DataRequired,Email
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,session
+from ..model import User
 
 
 class LoginForm(FlaskForm):
@@ -13,11 +14,20 @@ class LoginForm(FlaskForm):
 @auth.route('/login',methods=['GET','POST'])
 def login():
     loginForm = LoginForm()
+    errorMessage = None
     if loginForm.validate_on_submit():
         email=loginForm.email.data
         password = loginForm.password.data
-        print(email)
-        print(password)
-        return redirect(url_for('main.index'))
+        user=User.query.filter_by(email=email).first()
+        if user and user.verify_password(password):
+            session["username"] = user.username
+            session["know"] = True
+            return redirect(url_for('main.index'))
+        else:
+            session["username"] = None
+            session["know"] = False
+            errorMessage = "密碼錯誤"
+            print(errorMessage)
 
-    return render_template('/auth/login.html',form = loginForm),200
+
+    return render_template('/auth/login.html',form = loginForm,error=errorMessage),200
