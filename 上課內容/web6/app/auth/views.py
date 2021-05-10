5c4ee92a356637d21b7a68c96e704a4bacb8236a
roberthsu2003 <roberthsu2003@gmail.com>
2021-05-10 16:57:28 +0800

    modify

--- 上課內容/web6/app/auth/views.py
+++ 上課內容/web6/app/auth/views.py
@@ -2,7 +2,8 @@ from . import auth
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField,SubmitField,PasswordField
 from wtforms.validators import DataRequired,Email
-from flask import render_template
+from flask import render_template,redirect,url_for
+
 
 class LoginForm(FlaskForm):
     email = StringField("Email:",validators=[DataRequired(),Email()])
@@ -12,4 +13,11 @@ class LoginForm(FlaskForm):
 @auth.route('/login',methods=['GET','POST'])
 def login():
     loginForm = LoginForm()
+    if loginForm.validate_on_submit():
+        email=loginForm.email.data
+        password = loginForm.password.data
+        print(email)
+        print(password)
+        return redirect(url_for('main.index'))
+
     return render_template('/auth/login.html',form = loginForm),200
