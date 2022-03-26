74086cdae5fe112965094b20490594f201427535
徐國堂 <roberthsu2003@gmail.com>
2022-03-26 13:46:20 +0800

    modify

--- 星期六_flask/web1/main.py
+++ 星期六_flask/web1/main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
+
 import dataSource
 
 app = Flask(__name__)
@@ -7,9 +9,22 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Index Page'
+authorization = "rdec-key-123-45678-011121314"
 
 @app.route("/weather")
 def show_weather():
+    authName = request.args.get("Authorization")
+    if authName is None:
+        print("沒有Authorization")
+    else:
+        print(f"Authorization:{authName}")
+
+    format = request.args.get("format")
+    if format is None:
+        print("沒有format")
+    else:
+        print(f"format:{format}")
+
     weatherList = dataSource.get_weather_of_taiwan()
     return jsonify(weatherList)
 
