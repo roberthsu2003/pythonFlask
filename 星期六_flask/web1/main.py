4d3dff6577e2d639b90f7d6385157abd7a7287a0
徐國堂 <roberthsu2003@gmail.com>
2022-03-26 10:25:09 +0800

    modify

--- 星期六_flask/web1/main.py
+++ 星期六_flask/web1/main.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1>Hello, World!</H1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
