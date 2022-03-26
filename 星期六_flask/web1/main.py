b00913b320fe5438b9330c9357ea7094bfc32d87
徐國堂 <roberthsu2003@gmail.com>
2022-03-26 11:02:53 +0800

    modify

--- 星期六_flask/web1/main.py
+++ 星期六_flask/web1/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -13,6 +14,11 @@ def hello():
 @app.route('/world')
 def world():
     return "<h1>World</h>"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'使用者是:{escape(username)}'
 
 if __name__ == "__main__":
     app.run(debug=True)
