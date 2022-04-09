97c6d2ff5c552ea7bc7af3562a5961d51bc3a85a
徐國堂 <roberthsu2003@gmail.com>
2022-04-09 15:56:56 +0800

    modify

--- 星期六_flask/web2/main.py
+++ 星期六_flask/web2/main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template("a2.html")
 
+@app.route("/a3/")
+def index1():
+    return render_template("a3.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
