1e65ac824f99a3a08c4fd41c167320962de7169d
徐國堂 <roberthsu2003@gmail.com>
2022-04-09 10:14:02 +0800

    modify

--- 星期六_flask/web2/main.py
+++ 星期六_flask/web2/main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, Flask!</p>"
+def index():
+    return render_template("a1.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
