e521fd976c5e4fcdaa192a3ce335480b7b2f3e86
徐國堂 <roberthsu2003@gmail.com>
2022-04-09 10:27:54 +0800

    modify

--- 星期六_flask/web2/main.py
+++ 星期六_flask/web2/main.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("a1.html")
+    name = "Robert"
+    age = 18
+    return render_template("a1.html",name=name,age=age)
 
 if __name__ == "__main__":
     app.run(debug=True)
