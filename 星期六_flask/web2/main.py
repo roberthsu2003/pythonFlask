cb622ae976903efcb05fdddcdff5f044d68a1772
徐國堂 <roberthsu2003@gmail.com>
2022-04-16 11:58:15 +0800

    modify

--- 星期六_flask/web2/main.py
+++ 星期六_flask/web2/main.py
@@ -3,14 +3,12 @@ import dataSource
 
 
 app = Flask(__name__)
-conn = dataSource.create_connection('lot.db')
-if conn is not None:
-    with conn:  #自動close動作
-        print("連線成功")
-        dataSource.insertData(conn,('robert1',35,21,25,5,17,36,43,'2022-04-16 11:18:20'))
-
 @app.route("/")
 def index():
+    conn = dataSource.create_connection('lot.db')
+    if conn is not None:
+        with conn:
+            print("connection successful")
     return render_template("a2.html")
 
 @app.route("/a3/")
