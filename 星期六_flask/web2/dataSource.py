af1f662e1087eec06b9e91cf32ca09da389f50a7
徐國堂 <roberthsu2003@gmail.com>
2022-04-16 11:48:38 +0800

    modify

---
+++ 星期六_flask/web2/dataSource.py
@@ -0,0 +1,49 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_name):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_name)
+    except Error as e:
+        print('連結資料庫發生錯誤')
+        print(e)
+        return conn
+
+    create_table(conn)
+    return conn
+
+
+def create_table(conn):
+    sql = '''
+    CREATE TABLE IF NOT EXISTS lot(
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        num1 INTEGER NOT NULL,
+        num2 INTEGER NOT NULL,
+        num3 INTEGER NOT NULL,
+        num4 INTEGER NOT NULL,
+        num5 INTEGER NOT NULL,
+        num6 INTEGER NOT NULL,
+        spec INTEGER NOT NULL,
+        datetime TEXT
+    );
+    '''
+    try:
+        cursor = conn.cursor()
+        cursor.execute(sql)
+    except Error as e:
+        print('建立資料表出錯')
+        print(e)
+        return None
+
+    print('資料表建立')
+
+
+def insertData(conn, data_list):
+    insertSQL = '''INSERT INTO lot (name,num1,num2,num3,num4,num5,num6,spec,datetime) 
+VALUES (?,?,?,?,?,?,?,?,?);'''
+    cursor = conn.cursor()
+    cursor.execute(insertSQL, data_list)
+    conn.commit()
--- 星期六_flask/web2/main.py
+++ 星期六_flask/web2/main.py
@@ -1,7 +1,13 @@
 from flask import Flask,render_template
+import dataSource
 
 
 app = Flask(__name__)
+conn = dataSource.create_connection('lot.db')
+if conn is not None:
+    with conn:  #自動close動作
+        print("連線成功")
+        dataSource.insertData(conn,('robert1',35,21,25,5,17,36,43,'2022-04-16 11:18:20'))
 
 @app.route("/")
 def index():
