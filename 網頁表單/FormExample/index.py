64d607ba9aa47e641d829a4588f818fce98b65e5
徐國堂 <roberthsu2003@gmail.com>
2022-12-03 08:45:15 +0800

    modify

--- 網頁表單/FormExample/index.py
+++ 網頁表單/FormExample/index.py
@@ -1,7 +1,22 @@
 from flask import Flask,render_template
+import os
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.urandom(24)
 
-@app.route("/")
+class NameForm(FlaskForm):
+    name = StringField("請輸入姓名",validators=[DataRequired()])
+    submit = SubmitField("送出")
+
+@app.route("/",methods=["GET","POST"])
 def index():
-    return render_template("form1.html")
+    name   = None
+    form = NameForm()
+    print(form)
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("form1.html",form=form,name=name)
