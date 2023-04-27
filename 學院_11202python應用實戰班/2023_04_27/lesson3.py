d6904fe81ec76d84201516dffff071619cd6c876
roberthsu2003 <roberthsu2003@gmail.com>
2023-04-27 15:00:19 +0800

    modify

--- 學院_11202python應用實戰班/2023_04_27/lesson3.py
+++ 學院_11202python應用實戰班/2023_04_27/lesson3.py
@@ -6,8 +6,15 @@ app = Flask(__name__)
 def index():
     message = "<h1>request headers</h1>"
     message += f"<ul><li>host:{request.headers.get('Host')}</li>"
-    message += f"<li>host_url:{request.headers.get('host_url')}</li>"
+    message += f"<li>methos:{request.method}</li>"
     message += f"<li>user_agent:{request.headers.get('User-Agent')}</li>"
     message +=  f"<li>Accept-Language:{request.headers.get('Accept-Language')}</li></ul>"
     return message
-    
+
+@app.route("/user/<name>")
+def user(name):
+    return f"<h1>Hello!{name}</h1>"
+
+@app.route("/error")
+def error():
+    return "<h1>error</h1>",404
