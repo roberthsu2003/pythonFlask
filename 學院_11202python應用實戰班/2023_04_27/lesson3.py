fa4d34285018c2bb4339bfe24895b18b517a4e0b
roberthsu2003 <roberthsu2003@gmail.com>
2023-04-27 14:31:00 +0800

    modify

--- 學院_11202python應用實戰班/2023_04_27/lesson3.py
+++ 學院_11202python應用實戰班/2023_04_27/lesson3.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello! Flask!</h1>"
+    message = "<h1>request headers</h1>"
+    message += f"<ul><li>host:{request.headers.get('Host')}</li>"
+    message += f"<li>host_url:{request.headers.get('host_url')}</li>"
+    message += f"<li>user_agent:{request.headers.get('User-Agent')}</li>"
+    message +=  f"<li>Accept-Language:{request.headers.get('Accept-Language')}</li></ul>"
+    return message
+    
