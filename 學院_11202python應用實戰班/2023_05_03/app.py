fdf757212d5a3c077d2e2f7d2d99b1ef5a883708
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-03 11:15:43 +0800

    modify

--- 學院_11202python應用實戰班/2023_05_03/app.py
+++ 學院_11202python應用實戰班/2023_05_03/app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.jinja.html")
+
+@app.route("/learning/")
+def learning():
+    return render_template("learning.jinja.html")
