39c5dd67bbc1e04da96f27678919edcfefe9f1b2
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-05 14:07:37 +0800

    modify

--- 學院_11202python應用實戰班/2023_05_05/app.py
+++ 學院_11202python應用實戰班/2023_05_05/app.py
@@ -1,9 +1,10 @@
 from flask import Flask,render_template
-from flask import request
+
 
 app = Flask(__name__)
 @app.route("/")
 def index():
+    
     data = {
         "key1":'''台股受台積電、航運股跌勢拖累，今 (3) 日開盤下挫，早盤一度跌逾百點，儘管尾盤跌勢收斂，終場仍下跌 83.07 點，或 0.53% 至 15553.41 點，再度失守季線，成交量 2013.79 億元。''',
         "key2":'''電金權值股中，台積電失守 500 元關卡，下跌 1%，聯發科逆勢上漲 1.3%，日月光也走強上漲 1%，不過航運股今日跌勢兇猛，長榮下挫 3.54%、萬海 4.27%、陽明跌 1.94%。''',
@@ -15,10 +16,12 @@ def index():
 
 @app.route("/features/")
 def features():
+    
     return render_template("features.jinja.html")
 
 @app.route("/priceing/")
 def priceing():
+    
     return render_template("priceing.jinja.html")
 
 @app.route("/about/")
