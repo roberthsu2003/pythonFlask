718d3a415974667388a7f73c765b69c31fae2c8f
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-10 11:19:49 +0800

    modify

--- 學院_11202python應用實戰班/2023_05_05/app.py
+++ 學院_11202python應用實戰班/2023_05_05/app.py
@@ -5,9 +5,8 @@ import datasource
 app = Flask(__name__)
 @app.route("/")
 def index():
-    stock_data = datasource.get_stock_data(stockid=2303)
-    print(type(stock_data[0][0]))
-    return render_template("index.jinja.html")
+    stock_data = datasource.get_stock_data(stockid=2303)    
+    return render_template("index.jinja.html",stockid=2303,stock=stock_data)
 
 
 @app.route("/features/")
