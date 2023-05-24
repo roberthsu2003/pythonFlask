886f8a52191860d534c29510cd1068765a70296d
roberthsu2003 <roberthsu2003@gmail.com>
2023-05-24 16:15:22 +0800

    modify

--- 學院_11202python應用實戰班/2023_05_24/app.py
+++ 學院_11202python應用實戰班/2023_05_24/app.py
@@ -1,7 +1,15 @@
 from flask import Flask,render_template
+import plotly.express as px
+import plotly
+import json
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('index.jinja.html')
+    df = px.data.tips()
+    fig = px.scatter(df, x="total_bill", y="tip", trendline="ols")
+    graphJSON = json.dumps(fig,cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('index.jinja.html',graphJSON=graphJSON)
