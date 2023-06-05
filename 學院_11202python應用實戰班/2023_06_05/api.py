c18fa3759796844568424d5a667b9c2dc9c1bcaf
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-05 10:49:33 +0800

    modify

--- 學院_11202python應用實戰班/2023_06_05/api.py
+++ 學院_11202python應用實戰班/2023_06_05/api.py
@@ -1,12 +1,12 @@
 from flask import Blueprint
 
-api = Blueprint("api",__name__,url_prefix="/api")
+api1 = Blueprint("api",__name__,url_prefix="/api")
 
-@api.route("/")
+@api1.route("/")
 def api():
     return "Hello! 本目錄提供Web api"
 
-@api.route("/youbike")
+@api1.route("/youbike")
 def youbike():
     return "<h1>Hello! Youbike</h1>"
 
--- 學院_11202python應用實戰班/2023_06_05/app.py
+++ 學院_11202python應用實戰班/2023_06_05/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from api import api1
 
 app = Flask(__name__)
+app.register_blueprint(api1)
 
 @app.route("/")
 def index():
