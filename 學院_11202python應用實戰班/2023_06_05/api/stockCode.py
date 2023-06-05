91568dcb879d87c809cb517117694344843e064e
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-05 14:21:12 +0800

    modify

--- 學院_11202python應用實戰班/2023_06_05/api/stockCode.py
+++ 學院_11202python應用實戰班/2023_06_05/api/stockCode.py
@@ -9,3 +9,13 @@ def stockCode():
     all_list = code_dataframe1.values.tolist()
     python_list = [{item[0]:item[1]} for item in all_list]
     return json.dumps(python_list, ensure_ascii=False)
+
+@api1.route("/stockCode/<int:code>")
+def stockName(code):
+    code_dataframe = pd.read_csv("api/codeSearch.csv")
+    code_dataframe1 =code_dataframe[['code','name']]
+    try:
+        name = code_dataframe1.query('code == @code')['name'].values[0]
+    except IndexError:
+        return f"豬頭沒有{code}"
+    return name
