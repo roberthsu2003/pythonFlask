f7e97bce6b52ff52d84641aaff7bbaff0ac51ec0
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-07 14:26:19 +0800

    modify

--- 學院_11202python應用實戰班/2023_06_07/app.py
+++ 學院_11202python應用實戰班/2023_06_07/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def index():    
     return render_template('index.jinja.html')
 
-@app.route("/login")
+@app.route("/login",methods=['GET', 'POST'])
 def login():
     return render_template('login.jinja.html')
 
