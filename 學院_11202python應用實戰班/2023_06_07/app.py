14f49e4ab548eff9ac71c816e33cdd0d32a881d1
roberthsu2003 <roberthsu2003@gmail.com>
2023-06-07 14:51:28 +0800

    modify

--- 學院_11202python應用實戰班/2023_06_07/app.py
+++ 學院_11202python應用實戰班/2023_06_07/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,url_for,redirect
 
 app = Flask(__name__)
 @app.route("/")
@@ -7,5 +7,13 @@ def index():
 
 @app.route("/login",methods=['GET', 'POST'])
 def login():
-    return render_template('login.jinja.html')
+    error = None
+    if request.method == 'POST':
+        if request.form['email'] == 'robert@gmail.com' and request.form['pwd'] == '12345':
+            return redirect(url_for('index'))
+        else:
+            error = '密碼不正確'
+    
+    return render_template('login.jinja.html', error=error)
+    
 
